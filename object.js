82a26d1ba7c98f7b5e0a9389c5f6c096672e5e11
ILYA <106765406+D15ND@users.noreply.github.com>
2023-01-26 21:47:59 +0300

    feat: add strings

--- object.js
+++ object.js
@@ -9,9 +9,20 @@ const object = {
 
 object.heigth=259; //add new key in object
 
+const newKey='laptoop';
+const newValue='MacBook';
+object.newKey=newValue;
+object[newKey]=newValue;
+
+const properyName='name';
 
 console.log(object.name);
+console.log(object[properyName]);
 console.log(object.age);
 console.log(object.passpordId.series);
 console.log(object.passpordId.number);
 console.log(object)
+
+for (let key in object) {
+  console.log(key);
+}
