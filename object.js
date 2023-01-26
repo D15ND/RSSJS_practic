aa2806699bb9a1fcdf84b20067c358b4bfb69e2f
ILYA <106765406+D15ND@users.noreply.github.com>
2023-01-26 19:56:43 +0300

    feat: add object js file

---
+++ object.js
@@ -0,0 +1,13 @@
+const object = {
+  name: 'Ilya',
+  age: 25,
+  passpordId: {
+    series: 'MC',
+    number: 2471
+  }
+}
+
+console.log(object.name);
+console.log(object.age);
+console.log(object.passpordId.series);
+console.log(object.passpordId.number);
