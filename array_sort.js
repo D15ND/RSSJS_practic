e5a1b06ef50dda1d4d63d9e90117931eec3b682a
ILYA <106765406+D15ND@users.noreply.github.com>
2023-02-08 23:11:44 +0300

    feat: add js fiels

---
+++ array_sort.js
@@ -0,0 +1,5 @@
+let arr=[[1,2,3], [4,5,6], [7,8,9]];
+
+// let arrRev=arr[1].reverse();
+
+console.log(arr[0], arr[1].reverse(), arr[2]);
---
+++ bit_systeam.js
@@ -0,0 +1,5 @@
+function toBit(num) {
+  let bin=num.toString(2); //command for binary systeam(2-nd)
+  console.log(`${num} in binary systeam: ${bin}`);
+}
+toBit(9);
